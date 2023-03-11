914076413f18004e0a622b780049e58438c0f995
hwany <lopahn2@gmail.com>
2023-03-11 19:05:41 +0900

    fetch 23-03-11: group get 요청시 master id 추가 리턴

--- routes/group.js
+++ routes/group.js
@@ -61,7 +61,7 @@ router.post('/', verifyToken, async (req, res) => {
 
 router.get('/', verifyToken, async (req, res) => {
 	try {
-        const [groupsSelectReseult, fieldUser] = await conn.execute('SELECT id, room_name ,accommodation, `explain`, created_at FROM `group`');
+        const [groupsSelectReseult, fieldUser] = await conn.execute('SELECT id, master_id ,room_name ,accommodation, `explain`, created_at FROM `group`');
 		return res.status(200).json({
 			message : "전체 스터디그룹 정보를 전송합니다.",
             groupsSelectReseult
@@ -102,7 +102,7 @@ router.get('/user/:roomId', verifyToken, async (req, res) => {
 router.get('/:roomId', verifyToken, async (req, res) => {
 	try {
         const roomId = req.params.roomId;
-        const [groupSelectReseult, fieldUser] = await conn.execute('SELECT id, room_name,accommodation,`explain` ,created_at FROM `group` WHERE id = ?', [roomId]);
+        const [groupSelectReseult, fieldUser] = await conn.execute('SELECT id, master_id,room_name,accommodation,`explain` ,created_at FROM `group` WHERE id = ?', [roomId]);
 		return res.status(200).json({
 			message : "특정 스터디그룹 정보를 전송합니다.",
             groupSelectReseult
